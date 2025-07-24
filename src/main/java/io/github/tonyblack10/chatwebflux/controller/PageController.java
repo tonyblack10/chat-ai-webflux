da536abb07cd3db5f4bfb8693e2b71c486188785
Tony Costa <tonyblack10@gmail.com>
2025-07-24 06:46:11 -0300

    feat: add chatai.jte and main.jte for dashboard layout and structure

--- src/main/java/io/github/tonyblack10/chatwebflux/controller/PageController.java
+++ src/main/java/io/github/tonyblack10/chatwebflux/controller/PageController.java
@@ -30,7 +30,7 @@ public class PageController {
         model.put("timestamp", System.currentTimeMillis());
 
         StringOutput output = new StringOutput();
-        templateEngine.render("chat.jte", model, output);
+        templateEngine.render("pages/chatai.jte", model, output);
 
         return Mono.just(output.toString());
     }
